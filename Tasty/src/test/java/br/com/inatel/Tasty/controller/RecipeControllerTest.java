eb69520c56e7f85e23df231a63061c77a2f88643
mairaalvs <mairaalves@gec.inatel.br>
2023-01-06 15:29:07 -0300

    Tests

--- Tasty/src/test/java/br/com/inatel/Tasty/controller/RecipeControllerTest.java
+++ Tasty/src/test/java/br/com/inatel/Tasty/controller/RecipeControllerTest.java
@@ -3,16 +3,21 @@ package br.com.inatel.Tasty.controller;
 import br.com.inatel.Tasty.model.dto.RecipeEvaluationDto;
 import br.com.inatel.Tasty.model.entity.RecipeEvaluation;
 import com.intuit.karate.Json;
+import com.intuit.karate.JsonUtils;
 import org.json.JSONArray;
 import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.junit.jupiter.api.Order;
 
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,9 +27,17 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class RecipeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
     @Order(1)
@@ -40,7 +53,7 @@ public class RecipeControllerTest {
     @Test
     @Order(2)
     void givenAReadOrderByRecipeIdValid_WhenReceivingTheEvaluation_ThenItShouldReturnStatus200Ok() {
-        String recipeId = "chocolate cake";
+        String recipeId = "cakes";
 
         RecipeEvaluationDto[] recipeEvaluationDto = WebTestClient.bindToServer().baseUrl("http://localhost:8081/evaluation").build()
                 .get()
@@ -78,4 +91,13 @@ public class RecipeControllerTest {
         assertEquals(recipeEvaluationDto.length, 0);
     }
 
+    @Test
+    @Order(4)
+    void givenACleaningOrder_ThenItShouldReturnStatus204NoContent() {
+        WebTestClient.bindToServer().baseUrl("http://localhost:8081/evaluation").build()
+                .delete()
+                .uri("/cache")
+                .exchange()
+                .expectStatus().isNoContent();
+    }
 }
